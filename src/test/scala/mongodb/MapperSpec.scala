b7ed5feeb2c50c7b740c34e7f72f82767e98d07b
Max Afonov <max@bumnetworks.com>
2010-08-06 17:26:59 -0400

    @MappedBy is obsolete (left out of previous commit)

--- src/test/scala/mongodb/MapperSpec.scala
+++ src/test/scala/mongodb/MapperSpec.scala
@@ -38,14 +38,12 @@ import mongodb.mapper.Mapper
 import mongodb.mapper.annotations._
 
 @BeanInfo
-@MappedBy(classOf[Any])
 class Widget(@ID var name: String, @Key var price: Int) {
   def this() = this(null, 0)
   override def toString() = "Widget(" + name + ", " + price + ")"
 }
 
 @BeanInfo
-@MappedBy(classOf[Any])
 class Piggy {
   @ID(auto = true)
   var id: ObjectId = _
@@ -69,7 +67,6 @@ class Piggy {
 }
 
 @BeanInfo
-@MappedBy(classOf[Any])
 class Chair {
   @ID(auto = true)
   var id: ObjectId = _
@@ -82,7 +79,6 @@ class Chair {
 }
 
 @BeanInfo
-@MappedBy(classOf[Any])
 class Badge {
   @ID
   var name: String = _
