dccd11a8f14c1f05eb4ee1e31b189a788b65f230
Brendan W. McAdams <brendan@10gen.com>
2010-12-01 16:07:26 -0500

    Added Date tests to $Ne and added testing for $lt

--- casbah-query/src/test/scala/DSLCoreOperatorsSpec.scala
+++ casbah-query/src/test/scala/DSLCoreOperatorsSpec.scala
@@ -39,6 +39,8 @@ class DSLCoreOperatorsSpec extends Specification with PendingUntilFixed with Log
   "Casbah's DSL $ne operator" should {
     shareVariables
 
+    val testDate = new java.util.Date(109, 01, 02, 0, 0, 0)
+
     "Accept a right hand value of String" in {
       val neStr = "foo" $ne "ISBN-123456789"
       neStr must notBeNull
@@ -108,7 +110,7 @@ class DSLCoreOperatorsSpec extends Specification with PendingUntilFixed with Log
       }
     }
 
-    "Accept a right hand value of Numeric" in {
+    "Accept a right hand value of ValidDateOrNumericType" in {
       "with Int" in {
         val neInt = "foo" $ne 10
         neInt.toString // Test that JSON Serialization works
@@ -164,6 +166,176 @@ class DSLCoreOperatorsSpec extends Specification with PendingUntilFixed with Log
         neShort must notBeNull
         neShort must haveSuperClass[DBObject]
         neShort must beEqualTo(nonDSL("foo", "$ne", java.lang.Short.parseShort("10")))
+      }
+      "with JDKDate" in {
+        val neJDKDate = "foo" $ne testDate
+        neJDKDate.toString // Test that JSON Serialization works
+        neJDKDate must notBeNull
+        neJDKDate must haveSuperClass[DBObject]
+        neJDKDate must beEqualTo(nonDSL("foo", "$ne", testDate))
+      }
+      "with JodaDT" in {
+        RegisterJodaTimeConversionHelpers()
+        val neJodaDT = "foo" $ne new org.joda.time.DateTime(testDate.getTime)
+        neJodaDT.toString // Test that JSON Serialization works
+        neJodaDT must notBeNull
+        neJodaDT must haveSuperClass[DBObject]
+        neJodaDT must beEqualTo(nonDSL("foo", "$ne", new org.joda.time.DateTime(testDate.getTime)))
+        DeregisterJodaTimeConversionHelpers()
+      }
+
+    }
+  
+
+  }
+
+
+  "Casbah's DSL $lt operator" should {
+    shareVariables
+
+    val testDate = new java.util.Date(109, 01, 02, 0, 0, 0)
+
+    "Accept a right hand value of String" in {
+      val neStr = "foo" $lt "ISBN-123456789"
+      neStr must notBeNull
+      neStr must haveSuperClass[DBObject]
+      neStr must beEqualTo(nonDSL("foo", "$lt", "ISBN-123456789"))
+    }
+
+    "Accept a right hand value of DBObject" in {
+      "A BasicDBObject created value" in {
+        val neObj = "foo" $lt new BasicDBObject("bar", "baz")
+        neObj must notBeNull
+        neObj must haveSuperClass[DBObject]
+        neObj must beEqualTo(nonDSL("foo", "$lt", new BasicDBObject("bar", "baz")))
+      }
+      "A MongoDBObject created value" in {
+        val neObj = "foo" $lt MongoDBObject("bar" -> "baz")
+        neObj must notBeNull
+        neObj must haveSuperClass[DBObject]
+        neObj must beEqualTo(nonDSL("foo", "$lt", MongoDBObject("bar" -> "baz")))
+      }
+      "A DBList should work also" in {
+        val neLst = "foo" $lt MongoDBList("x", "y", "z")
+        neLst must notBeNull
+        neLst must haveSuperClass[DBObject]
+        neLst must beEqualTo(nonDSL("foo", "$lt", MongoDBList("x", "y", "z")))
+
+      }
+    }
+
+    "Accept List-like values descended from Iterable" in {
+      "An immutable List works" in {
+        import scala.collection.immutable.List
+        val neLst = "foo" $lt List("x", "y", 5)
+        neLst must notBeNull
+        neLst must haveSuperClass[DBObject]
+        neLst must beEqualTo(nonDSL("foo", "$lt", List("x", "y", 5)))
+      }
+
+      "An immutable Seq works" in {
+        import scala.collection.immutable.Seq
+        val neSeq = "foo" $lt Seq("x", "y", 5)
+        neSeq must notBeNull
+        neSeq must haveSuperClass[DBObject]
+        neSeq must beEqualTo(nonDSL("foo", "$lt", Seq("x", "y", 5)))
+      }
+
+      "An immutable Set works" in {
+        import scala.collection.immutable.Set
+        val neSet = "foo" $lt Set("x", "y", 5)
+        neSet must notBeNull
+        neSet must haveSuperClass[DBObject]
+        /*neSet must beEqualTo(nonDSL("foo", "$lt", Set("x", "y", 5)))*/
+      }
+
+      "An mutable HashSet works" in {
+        import scala.collection.mutable.HashSet
+        val neHashSet = "foo" $lt HashSet("x", "y", 5)
+        neHashSet must notBeNull
+        neHashSet must haveSuperClass[DBObject]
+        /*neHashSet must beEqualTo(nonDSL("foo", "$lt", HashSet("x", "y", 5)))*/
+      }
+
+      "Also, Arrays function" in {
+        val neArray = "foo" $lt Array("x", "y", 5)
+        neArray must notBeNull
+        neArray must haveSuperClass[DBObject]
+      }
+    }
+
+    "Accept a right hand value of ValidDateOrNumericType" in {
+      "with Int" in {
+        val neInt = "foo" $lt 10
+        neInt.toString // Test that JSON Serialization works
+        neInt must notBeNull
+        neInt must haveSuperClass[DBObject]
+        neInt must beEqualTo(nonDSL("foo", "$lt", 10))
+      }
+      "with BigDecimal" in {
+        val neBD = "foo" $lt BigDecimal("5.8233232")
+        neBD.toString // Test that JSON Serialization works
+        neBD must notBeNull
+        neBD must haveSuperClass[DBObject]
+        neBD must beEqualTo(nonDSL("foo", "$lt", BigDecimal("5.8233232")))
+      }
+      "with BigInt" in {
+        val neBI = "foo" $lt BigInt("1000000000000000000425425245252")
+        neBI.toString // Test that JSON Serialization works
+        neBI must notBeNull
+        neBI must haveSuperClass[DBObject]
+        neBI must beEqualTo(nonDSL("foo", "$lt",  BigInt("1000000000000000000425425245252")))
+      }
+      "with Byte" in {
+        val neByte = "foo" $lt java.lang.Byte.parseByte("51")
+        neByte.toString // Test that JSON Serialization works
+        neByte must notBeNull
+        neByte must haveSuperClass[DBObject]
+        neByte must beEqualTo(nonDSL("foo", "$lt", java.lang.Byte.parseByte("51")))
+      }
+      "with Double" in {
+        val neDouble = "foo" $lt 5.232352
+        neDouble.toString // Test that JSON Serialization works
+        neDouble must notBeNull
+        neDouble must haveSuperClass[DBObject]
+        neDouble must beEqualTo(nonDSL("foo", "$lt", 5.232352))
+      }
+      "with Float" in {
+        val neFloat = "foo" $lt java.lang.Float.parseFloat("5.232352")
+        neFloat.toString // Test that JSON Serialization works
+        neFloat must notBeNull
+        neFloat must haveSuperClass[DBObject]
+        neFloat must beEqualTo(nonDSL("foo", "$lt", java.lang.Float.parseFloat("5.232352")))
+      }
+      "with Long" in {
+        val neLong = "foo" $lt 10L
+        neLong.toString // Test that JSON Serialization works
+        neLong must notBeNull
+        neLong must haveSuperClass[DBObject]
+        neLong must beEqualTo(nonDSL("foo", "$lt", 10L))
+      }
+      "with Short" in {
+        val neShort = "foo" $lt java.lang.Short.parseShort("10")
+        neShort.toString // Test that JSON Serialization works
+        neShort must notBeNull
+        neShort must haveSuperClass[DBObject]
+        neShort must beEqualTo(nonDSL("foo", "$lt", java.lang.Short.parseShort("10")))
+      }
+      "with JDKDate" in {
+        val neJDKDate = "foo" $lt testDate
+        neJDKDate.toString // Test that JSON Serialization works
+        neJDKDate must notBeNull
+        neJDKDate must haveSuperClass[DBObject]
+        neJDKDate must beEqualTo(nonDSL("foo", "$lt", testDate))
+      }
+      "with JodaDT" in {
+        RegisterJodaTimeConversionHelpers()
+        val neJodaDT = "foo" $lt new org.joda.time.DateTime(testDate.getTime)
+        neJodaDT.toString // Test that JSON Serialization works
+        neJodaDT must notBeNull
+        neJodaDT must haveSuperClass[DBObject]
+        neJodaDT must beEqualTo(nonDSL("foo", "$lt", new org.joda.time.DateTime(testDate.getTime)))
+        DeregisterJodaTimeConversionHelpers()
       }
 
     }
