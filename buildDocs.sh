b39e8292e194bcacf3cdbf0478966ee238951fe4
Brendan W. McAdams <brendan@10gen.com>
2010-12-09 12:40:59 -0500

    Removed $eq operator as it was a test spike and seems to be causing possible external issues

--- buildDocs.sh
+++ buildDocs.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
-rm -rf docs
-mkdir -p docs/casbah-commons
-mkdir -p docs/casbah-core
-mkdir -p docs/casbah-gridfs
-mkdir -p docs/casbah-query
+rm -rf docs/scaladoc
+mkdir -p docs/scaladoc/casbah-commons
+mkdir -p docs/scaladoc/casbah-core
+mkdir -p docs/scaladoc/casbah-gridfs
+mkdir -p docs/scaladoc/casbah-query
 
 sbt "+update" "+clean" "+doc"
 
-cp -R casbah-commons/target/scala_2.8.1/classes.sxr docs/casbah-commons/sxr
-cp -R casbah-commons/target/scala_2.8.1/doc/main/api docs/casbah-commons/api
-cp -R casbah-core/target/scala_2.8.1/classes.sxr docs/casbah-core/sxr
-cp -R casbah-core/target/scala_2.8.1/doc/main/api docs/casbah-core/api
-cp -R casbah-gridfs/target/scala_2.8.1/classes.sxr docs/casbah-gridfs/sxr
-cp -R casbah-gridfs/target/scala_2.8.1/doc/main/api docs/casbah-gridfs/api
-cp -R casbah-query/target/scala_2.8.1/classes.sxr docs/casbah-query/sxr
-cp -R casbah-query/target/scala_2.8.1/doc/main/api docs/casbah-query/api
+cp -R casbah-commons/target/scala_2.8.1/classes.sxr docs/scaladoc/casbah-commons/sxr
+cp -R casbah-commons/target/scala_2.8.1/doc/main/api docs/scaladoc/casbah-commons/api
+cp -R casbah-core/target/scala_2.8.1/classes.sxr docs/scaladoc/casbah-core/sxr
+cp -R casbah-core/target/scala_2.8.1/doc/main/api docs/scaladoc/casbah-core/api
+cp -R casbah-gridfs/target/scala_2.8.1/classes.sxr docs/scaladoc/casbah-gridfs/sxr
+cp -R casbah-gridfs/target/scala_2.8.1/doc/main/api docs/scaladoc/casbah-gridfs/api
+cp -R casbah-query/target/scala_2.8.1/classes.sxr docs/scaladoc/casbah-query/sxr
+cp -R casbah-query/target/scala_2.8.1/doc/main/api docs/scaladoc/casbah-query/api
 
-cp doc_index.html docs/index.html
+cp doc_index.html docs/scaladoc/index.html
 
-cd docs
-perl -p -i -e 's#a href="http://api.mongodb.org/scala/casbah-(.*)/casbah-(.*)/sxr/.*/casbah-\2/.*src/main/scala/(.*)"#a href="/scala/casbah/\1/casbah-\2/sxr/\3.scala.html"#gi' `find ./ -name \*.html`
+cd docs/scaladoc
+perl -p -i -e 's#a href="http://api.mongodb.org/scala/casbah-(.*)/casbah-(.*)/sxr/.*/casbah-\2/.*src/main/scala/(.*)"#a href="/scala/casbah/\1/scaladoc/casbah-\2/sxr/\3.scala.html"#gi' `find ./ -name \*.html`
 
