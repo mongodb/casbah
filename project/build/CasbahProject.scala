82aa31bccb9c4900c0394691a66f0a1681729bef
Brendan W. McAdams <brendan@10gen.com>
2011-05-31 08:53:46 -0700

    Crossbuild updates

--- project/build/CasbahProject.scala
+++ project/build/CasbahProject.scala
@@ -86,10 +86,11 @@ class CasbahProject(info: ProjectInfo)
       CompoundDocOption("-doc-title", "Casbah %s".format(projectName.value))
     ) 
     // Testing Deps
-   val specs2 = "org.specs2" %% "specs2" % "1.3" % "test->default"
+    val specs2 = "org.specs2" %% "specs2" % "1.3"
+//    val scalaz = "org.specs2" %% "specs2-scalaz-core" % "6.0.RC2"
 
-   def specs2Framework = new TestFramework("org.specs2.runner.SpecsFramework")
-   override def testFrameworks = super.testFrameworks ++ Seq(specs2Framework)
+    def specs2Framework = new TestFramework("org.specs2.runner.SpecsFramework")
+    override def testFrameworks = super.testFrameworks ++ Seq(specs2Framework)
   }
 
   class CasbahUtilProject(info: ProjectInfo) extends CasbahBaseProject(info) {
