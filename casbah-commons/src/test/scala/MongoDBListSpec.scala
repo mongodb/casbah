97388b11abb16a4b3cef24dcacc6c560ab9949c0
Ilya Korniyko <intracer@gmail.com>
2012-10-30 20:33:54 +0200

    test MongoDBList.concat mutable traversable

--- casbah-commons/src/test/scala/MongoDBListSpec.scala
+++ casbah-commons/src/test/scala/MongoDBListSpec.scala
@@ -24,7 +24,6 @@ package com.mongodb.casbah.test.commons
 
 import com.mongodb.casbah.commons.Imports._
 import com.mongodb.casbah.commons.test.CasbahMutableSpecification
-import scalaj.collection.Imports._
 
 class MongoDBListSpec extends CasbahMutableSpecification {
 
@@ -92,6 +91,12 @@ class MongoDBListSpec extends CasbahMutableSpecification {
 
     "Concat immutable traversable" in {
       val dbList = MongoDBList.concat(List("ponies"))
+      dbList must haveSize(1)
+      dbList must beEqualTo(List("ponies"))
+    }
+
+    "Concat mutable traversable" in {
+      val dbList = MongoDBList.concat(collection.mutable.Buffer("ponies"))
       dbList must haveSize(1)
       dbList must beEqualTo(List("ponies"))
     }
