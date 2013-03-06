b756bb0f653f95d14bbd01c19e9398e8bc7d8e40
Ross Lawley <ross.lawley@gmail.com>
2013-03-06 11:31:04 +0000

    Updated version

--- project/CasbahBuild.scala
+++ project/CasbahBuild.scala
@@ -9,7 +9,7 @@ object CasbahBuild extends Build {
   lazy val buildSettings = Seq(
     organization := "org.mongodb",
     organizationHomepage := Some(url("http://www.mongodb.org")),
-    version      := "2.5.0",
+    version      := "2.5.1-SNAPSHOT",
     scalaVersion := "2.10.0",
     crossScalaVersions := Seq("2.10.0", "2.9.3", "2.9.2", "2.9.1")
   )
