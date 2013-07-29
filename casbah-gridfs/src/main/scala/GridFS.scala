1f40ec515c6f41935b8acd2c7f16b385b055c304
Ross Lawley <ross.lawley@gmail.com>
2013-07-29 09:29:42 +0000

    Handle JodaDateTimes in GridFS SCALA-113

--- casbah-gridfs/src/main/scala/GridFS.scala
+++ casbah-gridfs/src/main/scala/GridFS.scala
@@ -6,7 +6,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,28 +16,22 @@
  *
  * For questions and comments about this product, please see the project page at:
  *
- *     http://github.com/mongodb/casbah
+ * http://github.com/mongodb/casbah
  *
  */
 package com.mongodb.casbah
 package gridfs
 
+import java.io.{File, InputStream}
+import scala.beans.BeanInfo
+
+import com.mongodb.gridfs.{GridFS => MongoGridFS, GridFSDBFile => MongoGridFSDBFile,
+                           GridFSFile => MongoGridFSFile, GridFSInputFile => MongoGridFSInputFile}
+
 import com.mongodb.casbah.Imports._
 import com.mongodb.casbah.gridfs.Imports._
 import com.mongodb.casbah.commons.Logging
-import com.mongodb.casbah.commons.conversions.scala._
-
-import com.mongodb.DBObject
-import com.mongodb.gridfs.{ GridFS => MongoGridFS, GridFSDBFile => MongoGridFSDBFile, GridFSFile => MongoGridFSFile, GridFSInputFile => MongoGridFSInputFile }
-
-import java.io._
-
-import scala.reflect._
-import scala.collection.JavaConverters._
-
-import com.github.nscala_time.time.Imports._
-
-// todo - look into potential naming conflicts...
+import com.github.nscala_time.time.Imports.{DateTime, LocalDateTime}
 
 /**
  * Companion object for GridFS.
@@ -59,39 +53,30 @@ object GridFS extends Logging {
 
 }
 
-class GridFS protected[gridfs] (val underlying: MongoGridFS) extends Iterable[GridFSDBFile] with Logging {
-  log.info("Instantiated a new GridFS instance against '%s'", underlying)
+class GridFS protected[gridfs](override val underlying: MongoGridFS) extends GenericGridFS with Iterable[GridFSDBFile] {
 
-  type FileOp = GridFSFile => Unit
   type FileWriteOp = GridFSInputFile => Unit
-  type FileReadOp = GridFSDBFile => Unit
-
-  implicit val db = underlying.getDB().asScala
 
   def iterator = new Iterator[GridFSDBFile] {
     val fileSet = files
+
     def count() = fileSet.count
-    override def length() = fileSet.length
+
+    override def length = fileSet.length
+
     def numGetMores() = fileSet.numGetMores
+
     def numSeen() = fileSet.numSeen
 
     def curr() = new GridFSDBFile(fileSet.next().asInstanceOf[MongoGridFSDBFile])
+
     def explain() = fileSet.explain
 
-    def next() = new GridFSDBFile(fileSet.next.asInstanceOf[MongoGridFSDBFile])
+    def next() = new GridFSDBFile(fileSet.next().asInstanceOf[MongoGridFSDBFile])
+
     def hasNext: Boolean = fileSet.hasNext
   }
 
-  /**
-   * loan
-   *
-   * Basic implementation of the Loan pattern -
-   * the idea is to pass a Unit returning function
-   * and a Mongo file handle, and work on it within
-   * a code block.
-   *
-   */
-  protected[gridfs] def loan[T <: GenericGridFSFile](file: T)(op: T => Option[AnyRef]) = op(file)
   /**
    * Create a new GridFS File from a scala.io.Source
    *
@@ -160,190 +145,94 @@ class GridFS protected[gridfs] (val underlying: MongoGridFS) extends Iterable[Gr
    * TODO - Should the curried versions give the option to not automatically save?
    */
   def createFile(data: scala.io.Source): GridFSInputFile = throw new UnsupportedOperationException("Currently no support for scala.io.Source")
-  def withNewFile(data: scala.io.Source)(op: FileWriteOp) = throw new UnsupportedOperationException("Currently no support for scala.io.Source")
 
   def createFile(data: Array[Byte]): GridFSInputFile = underlying.createFile(data)
-  /**
-   * Loan pattern style file creation.
-   * @return The ID of the created File (Option[AnyRef])
-   */
-  def withNewFile(data: Array[Byte])(op: FileWriteOp) = loan(createFile(data)) { fh =>
-    op(fh)
-    fh.save()
-    fh.validate()
-    Option(fh.id)
-  }
 
   def createFile(f: File): GridFSInputFile = underlying.createFile(f)
-  /**
-   * Loan pattern style file creation.
-   * @return The ID of the created File (Option[AnyRef])
-   */
-  def withNewFile(f: File)(op: FileWriteOp) = loan(createFile(f)) { fh =>
-    op(fh)
-    fh.save()
-    fh.validate()
-    Option(fh.id)
-  }
 
   def createFile(in: InputStream): GridFSInputFile = underlying.createFile(in)
 
-  /**
-   * Loan pattern style file creation.
-   * @return The ID of the created File (Option[AnyRef])
-   */
-  def withNewFile(in: InputStream)(op: FileWriteOp) = loan(createFile(in)) { fh =>
-    op(fh)
-    fh.save()
-    fh.validate()
-    Option(fh.id)
-  }
-
   def createFile(in: InputStream, filename: String): GridFSInputFile = underlying.createFile(in, filename)
+
+  def withNewFile(data: scala.io.Source)(op: FileWriteOp) = throw new UnsupportedOperationException("Currently no support for scala.io.Source")
+
   /**
    * Loan pattern style file creation.
    * @return The ID of the created File (Option[AnyRef])
    */
-  def withNewFile(in: InputStream, filename: String)(op: FileWriteOp) = loan(createFile(in, filename)) { fh =>
-    op(fh)
-    fh.save()
-    fh.validate()
-    Option(fh.id)
+  def withNewFile(data: Array[Byte])(op: FileWriteOp) = loan(createFile(data)) {
+    fh =>
+      op(fh)
+      fh.save()
+      fh.validate()
+      Option(fh.id)
   }
 
+  /**
+   * Loan pattern style file creation.
+   * @return The ID of the created File (Option[AnyRef])
+   */
+  def withNewFile(f: File)(op: FileWriteOp) = loan(createFile(f)) {
+    fh =>
+      op(fh)
+      fh.save()
+      fh.validate()
+      Option(fh.id)
+  }
 
+  /**
+   * Loan pattern style file creation.
+   * @return The ID of the created File (Option[AnyRef])
+   */
+  def withNewFile(in: InputStream)(op: FileWriteOp) = loan(createFile(in)) {
+    fh =>
+      op(fh)
+      fh.save()
+      fh.validate()
+      Option(fh.id)
+  }
 
-  /** Find by query - returns a list */
-  def find[A <% DBObject](query: A) = underlying.find(query).asScala
-  /** Find by query - returns a single item */
-  def find(id: ObjectId): GridFSDBFile = underlying.find(id)
-  /** Find by query - returns a list */
-  def find(filename: String) = underlying.find(filename).asScala
+  /**
+   * Loan pattern style file creation.
+   * @return The ID of the created File (Option[AnyRef])
+   */
+  def withNewFile(in: InputStream, filename: String)(op: FileWriteOp) = loan(createFile(in, filename)) {
+    fh =>
+      op(fh)
+      fh.save()
+      fh.validate()
+      Option(fh.id)
+  }
 
   def findOne[A <% DBObject](query: A): Option[GridFSDBFile] = {
-    underlying.findOne(query) match {
-      case null => None
-      case x => Some(x)
-    }
-  }
-  def findOne(id: ObjectId): Option[GridFSDBFile] = {
-    underlying.findOne(id) match {
-      case null => None
-      case x => Some(x)
-    }
-  }
-  def findOne(filename: String): Option[GridFSDBFile] = {
-    underlying.findOne(filename) match {
-      case null => None
-      case x => Some(x)
+    filesCollection.findOne(query) match {
+      case None => None
+      case x => Some(new GridFSDBFile(x.get.asInstanceOf[MongoGridFSDBFile]))
     }
   }
 
-  def bucketName = underlying.getBucketName
+  def findOne(id: ObjectId): Option[GridFSDBFile] = findOne(MongoDBObject("_id" -> id))
 
-  /**
-   * Returns a cursor for this filestore
-   * of all of the files...
-   */
-  def files = { new MongoCursor(underlying.getFileList) }
-  def files[A <% DBObject](query: A) = { new MongoCursor(underlying.getFileList(query)) }
+  def findOne(filename: String): Option[GridFSDBFile] = findOne(MongoDBObject("filename" -> filename))
 
-  def remove[A <% DBObject](query: A) = underlying.remove(query)
-  def remove(id: ObjectId) = underlying.remove(id)
-  def remove(filename: String) = underlying.remove(filename)
 }
 
-@BeanInfo
-abstract class GenericGridFSFile(override val underlying: MongoGridFSFile) extends MongoDBObject with Logging {
-  type DateType
-
-  def convertDate(in: AnyRef): DateType
-
-  override def iterator = underlying.keySet.asScala.map { k =>
-    k -> underlying.get(k)
-  }.toMap.iterator.asInstanceOf[Iterator[(String, AnyRef)]]
-
-  def save() { underlying.save() }
-
-  /**
-   * validate the object.
-   * Throws an exception if it fails
-   * @throws MongoException An error describing the validation failure
-   */
-  def validate() { underlying.validate() }
-
-  def numChunks: Int = underlying.numChunks
-
-  def id = underlying.getId
-  def filename = Option(underlying.getFilename)
-  // todo - does mongo support mime magic? Should we pull some in here?
-  def contentType = Option(underlying.getContentType)
-  def length = underlying.getLength
-  def chunkSize = underlying.getChunkSize
-  def uploadDate: DateType = convertDate(underlying.get("uploadDate"))
-  def aliases = underlying.getAliases.asScala
-  def metaData: DBObject = underlying.getMetaData
-  def metaData_=[A <% DBObject](meta: A) = underlying.setMetaData(meta)
-  def md5: String = underlying.getMD5
-
-
-
-  override def toString = "{ GridFSFile(id=%s, filename=%s, contentType=%s) }".
-    format(id, filename, contentType)
-}
 
 @BeanInfo
-class GridFSFile(_underlying: MongoGridFSFile) extends GenericGridFSFile(_underlying) {
+class GridFSFile(_underlying: MongoGridFSFile) extends GenericGridFSFile(_underlying) with ConvertToDate
+
+@BeanInfo
+class GridFSDBFile(_underlying: MongoGridFSDBFile) extends GenericGridFSDBFile(_underlying) with ConvertToDate
+
+@BeanInfo
+class GridFSInputFile(_underlying: MongoGridFSInputFile) extends GenericGridFSInputFile(_underlying) with ConvertToDate
+
+trait ConvertToDate {
   type DateType = java.util.Date
-  def convertDate(in: AnyRef) = in match {
-    case d: java.util.Date => d
-    case j: DateTime => new java.util.Date(j.getMillis)
-  }
-}
 
-
-
-@BeanInfo
-abstract class GenericGridFSDBFile protected[gridfs] (override val underlying: MongoGridFSDBFile) extends GenericGridFSFile(underlying) {
-
-  def inputStream = underlying.getInputStream
-
-  def source = scala.io.Source.fromInputStream(inputStream)
-
-  def writeTo(file: java.io.File) = underlying.writeTo(file)
-
-  def writeTo(out: java.io.OutputStream) = underlying.writeTo(out)
-
-  def writeTo(filename: String) = underlying.writeTo(filename)
-
-  override def toString = "{ GridFSDBFile(id=%s, filename=%s, contentType=%s) }".
-    format(id, filename, contentType)
-}
-
-@BeanInfo
-class GridFSDBFile(_underlying: MongoGridFSDBFile) extends GenericGridFSDBFile(_underlying) {
-  type DateType = java.util.Date
   def convertDate(in: AnyRef): DateType = in match {
     case d: java.util.Date => d
-    case j: DateTime => new java.util.Date(j.getMillis)
+    case j: DateTime => j.toDate
+    case l: LocalDateTime => l.toDateTime.toDate
   }
 }
-
-
-@BeanInfo
-abstract class GenericGridFSInputFile protected[gridfs] (override val underlying: MongoGridFSInputFile) extends GenericGridFSFile(underlying) {
-  def filename_=(name: String) = underlying.setFilename(name)
-  def contentType_=(cT: String) = underlying.setContentType(cT)
-}
-
-
-@BeanInfo
-class GridFSInputFile(_underlying: MongoGridFSInputFile) extends GenericGridFSInputFile(_underlying) {
-  type DateType = java.util.Date
-  def convertDate(in: AnyRef): DateType = in match {
-    case d: java.util.Date => d
-    case j: DateTime => new java.util.Date(j.getMillis)
-  }
-}
-
-
--- casbah-gridfs/src/main/scala/JodaGridFS.scala
+++ casbah-gridfs/src/main/scala/JodaGridFS.scala
@@ -6,7 +6,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,27 +16,24 @@
  *
  * For questions and comments about this product, please see the project page at:
  *
- *     http://github.com/mongodb/casbah
+ * http://github.com/mongodb/casbah
  *
  */
 
 package com.mongodb.casbah
 package gridfs
 
+import java.io.{File, InputStream}
+import scala.beans.BeanInfo
+
+import com.mongodb.gridfs.{GridFS => MongoGridFS, GridFSDBFile => MongoGridFSDBFile,
+                           GridFSFile => MongoGridFSFile, GridFSInputFile => MongoGridFSInputFile}
+
 import com.mongodb.casbah.Imports._
 import com.mongodb.casbah.gridfs.Imports._
 import com.mongodb.casbah.commons.Logging
-import com.mongodb.casbah.commons.conversions.scala._
+import com.github.nscala_time.time.Imports.{DateTime, LocalDateTime}
 
-import com.mongodb.DBObject
-import com.mongodb.gridfs.{ GridFS => MongoGridFS, GridFSDBFile => MongoGridFSDBFile, GridFSFile => MongoGridFSFile, GridFSInputFile => MongoGridFSInputFile }
-
-import java.io._
-
-import scala.reflect._
-import scala.collection.JavaConverters._
-
-import com.github.nscala_time.time.Imports._
 
 /**
  * Companion object for GridFS.
@@ -59,39 +56,30 @@ object JodaGridFS extends Logging {
 }
 
 
-class JodaGridFS protected[gridfs] (val underlying: MongoGridFS) extends Iterable[JodaGridFSDBFile] with Logging {
-  log.info("Instantiated a new GridFS instance against '%s'", underlying)
+class JodaGridFS protected[gridfs](val underlying: MongoGridFS) extends GenericGridFS with Iterable[JodaGridFSDBFile] {
 
-  type FileOp = JodaGridFSFile => Unit
   type FileWriteOp = JodaGridFSInputFile => Unit
-  type FileReadOp = JodaGridFSDBFile => Unit
-
-  implicit val db = underlying.getDB().asScala
 
   def iterator = new Iterator[JodaGridFSDBFile] {
     val fileSet = files
+
     def count() = fileSet.count
-    override def length() = fileSet.length
+
+    override def length = fileSet.length
+
     def numGetMores() = fileSet.numGetMores
+
     def numSeen() = fileSet.numSeen
 
     def curr() = new JodaGridFSDBFile(fileSet.next().asInstanceOf[MongoGridFSDBFile])
+
     def explain() = fileSet.explain
 
-    def next() = new JodaGridFSDBFile(fileSet.next.asInstanceOf[MongoGridFSDBFile])
+    def next() = new JodaGridFSDBFile(fileSet.next().asInstanceOf[MongoGridFSDBFile])
+
     def hasNext: Boolean = fileSet.hasNext
   }
 
-  /**
-   * loan
-   *
-   * Basic implementation of the Loan pattern -
-   * the idea is to pass a Unit returning function
-   * and a Mongo file handle, and work on it within
-   * a code block.
-   *
-   */
-  protected[gridfs] def loan[T <: GenericGridFSFile](file: T)(op: T => Option[AnyRef]) = op(file)
   /**
    * Create a new GridFS File from a scala.io.Source
    *
@@ -160,145 +148,93 @@ class JodaGridFS protected[gridfs] (val underlying: MongoGridFS) extends Iterabl
    * TODO - Should the curried versions give the option to not automatically save?
    */
   def createFile(data: scala.io.Source): JodaGridFSInputFile = throw new UnsupportedOperationException("Currently no support for scala.io.Source")
-  def withNewFile(data: scala.io.Source)(op: FileWriteOp) = throw new UnsupportedOperationException("Currently no support for scala.io.Source")
 
   def createFile(data: Array[Byte]): JodaGridFSInputFile = underlying.createFile(data)
-  /**
-   * Loan pattern style file creation.
-   * @return The ID of the created File (Option[AnyRef])
-   */
-  def withNewFile(data: Array[Byte])(op: FileWriteOp) = loan(createFile(data)) { fh =>
-    op(fh)
-    fh.save()
-    fh.validate()
-    Option(fh.id)
-  }
 
   def createFile(f: File): JodaGridFSInputFile = underlying.createFile(f)
-  /**
-   * Loan pattern style file creation.
-   * @return The ID of the created File (Option[AnyRef])
-   */
-  def withNewFile(f: File)(op: FileWriteOp) = loan(createFile(f)) { fh =>
-    op(fh)
-    fh.save()
-    fh.validate()
-    Option(fh.id)
-  }
 
   def createFile(in: InputStream): JodaGridFSInputFile = underlying.createFile(in)
 
-  /**
-   * Loan pattern style file creation.
-   * @return The ID of the created File (Option[AnyRef])
-   */
-  def withNewFile(in: InputStream)(op: FileWriteOp) = loan(createFile(in)) { fh =>
-    op(fh)
-    fh.save()
-    fh.validate()
-    Option(fh.id)
-  }
-
   def createFile(in: InputStream, filename: String): JodaGridFSInputFile = underlying.createFile(in, filename)
+
+  def withNewFile(data: scala.io.Source)(op: FileWriteOp) = throw new UnsupportedOperationException("Currently no support for scala.io.Source")
+
   /**
    * Loan pattern style file creation.
    * @return The ID of the created File (Option[AnyRef])
    */
-  def withNewFile(in: InputStream, filename: String)(op: FileWriteOp) = loan(createFile(in, filename)) { fh =>
-    op(fh)
-    fh.save()
-    fh.validate()
-    Option(fh.id)
+  def withNewFile(data: Array[Byte])(op: FileWriteOp) = loan(createFile(data)) {
+    fh =>
+      op(fh)
+      fh.save()
+      fh.validate()
+      Option(fh.id)
   }
 
+  /**
+   * Loan pattern style file creation.
+   * @return The ID of the created File (Option[AnyRef])
+   */
+  def withNewFile(f: File)(op: FileWriteOp) = loan(createFile(f)) {
+    fh =>
+      op(fh)
+      fh.save()
+      fh.validate()
+      Option(fh.id)
+  }
 
+  /**
+   * Loan pattern style file creation.
+   * @return The ID of the created File (Option[AnyRef])
+   */
+  def withNewFile(in: InputStream)(op: FileWriteOp) = loan(createFile(in)) {
+    fh =>
+      op(fh)
+      fh.save()
+      fh.validate()
+      Option(fh.id)
+  }
 
-  /** Find by query - returns a list */
-  def find[A <% DBObject](query: A) = underlying.find(query).asScala
-  /** Find by query - returns a single item */
-  def find(id: ObjectId): JodaGridFSDBFile = underlying.find(id)
-  /** Find by query - returns a list */
-  def find(filename: String) = underlying.find(filename).asScala
+  /**
+   * Loan pattern style file creation.
+   * @return The ID of the created File (Option[AnyRef])
+   */
+  def withNewFile(in: InputStream, filename: String)(op: FileWriteOp) = loan(createFile(in, filename)) {
+    fh =>
+      op(fh)
+      fh.save()
+      fh.validate()
+      Option(fh.id)
+  }
 
   def findOne[A <% DBObject](query: A): Option[JodaGridFSDBFile] = {
-    underlying.findOne(query) match {
-      case null => None
-      case x => Some(x)
-    }
-  }
-  def findOne(id: ObjectId): Option[JodaGridFSDBFile] = {
-    underlying.findOne(id) match {
-      case null => None
-      case x => Some(x)
-    }
-  }
-  def findOne(filename: String): Option[JodaGridFSDBFile] = {
-    underlying.findOne(filename) match {
-      case null => None
-      case x => Some(x)
+    filesCollection.findOne(query) match {
+      case None => None
+      case x => Some(new JodaGridFSDBFile(x.get.asInstanceOf[MongoGridFSDBFile]))
     }
   }
 
-  def bucketName = underlying.getBucketName
+  def findOne(id: ObjectId): Option[JodaGridFSDBFile] = findOne(MongoDBObject("_id" -> id))
 
-  /**
-   * Returns a cursor for this filestore
-   * of all of the files...
-   */
-  def files = { new MongoCursor(underlying.getFileList) }
-  def files[A <% DBObject](query: A) = { new MongoCursor(underlying.getFileList(query)) }
+  def findOne(filename: String): Option[JodaGridFSDBFile] = findOne(MongoDBObject("filename" -> filename))
 
-  def remove[A <% DBObject](query: A) = underlying.remove(query)
-  def remove(id: ObjectId) = underlying.remove(id)
-  def remove(filename: String) = underlying.remove(filename)
-}
-
-
-
-@BeanInfo
-class JodaGridFSDBFile(_underlying: MongoGridFSDBFile) extends GenericGridFSDBFile(_underlying) {
-  type DateType = DateTime
-  def convertDate(in: AnyRef): DateType = in match {
-    case d: java.util.Date => new DateTime(d)
-    case j: DateTime => j
-  }
-  override def put(key: String, v: AnyRef) = {
-    val _v = v match {
-      case j: DateTime => new java.util.Date(j.getMillis)
-      case default => default
-    }
-    super.put(key, _v)
-  }
 }
 
 @BeanInfo
-class JodaGridFSFile(_underlying: MongoGridFSFile) extends GenericGridFSFile(_underlying) {
-  type DateType = DateTime
-  def convertDate(in: AnyRef): DateType = in match {
-    case d: java.util.Date => new DateTime(d)
-    case j: DateTime => j
-  }
-  override def put(key: String, v: AnyRef) = {
-    val _v = v match {
-      case j: DateTime => new java.util.Date(j.getMillis)
-      case default => default
-    }
-    super.put(key, _v)
-  }
-}
+class JodaGridFSDBFile(_underlying: MongoGridFSDBFile) extends GenericGridFSDBFile(_underlying) with ConvertToDateTime
 
 @BeanInfo
-class JodaGridFSInputFile(_underlying: MongoGridFSInputFile) extends GenericGridFSInputFile(_underlying) {
+class JodaGridFSFile(_underlying: MongoGridFSFile) extends GenericGridFSFile(_underlying) with ConvertToDateTime
+
+@BeanInfo
+class JodaGridFSInputFile(_underlying: MongoGridFSInputFile) extends GenericGridFSInputFile(_underlying) with ConvertToDateTime
+
+trait ConvertToDateTime {
   type DateType = DateTime
+
   def convertDate(in: AnyRef): DateType = in match {
     case d: java.util.Date => new DateTime(d)
     case j: DateTime => j
-  }
-  override def put(key: String, v: AnyRef) = {
-    val _v = v match {
-      case j: DateTime => new java.util.Date(j.getMillis)
-      case default => default
-    }
-    super.put(key, _v)
+    case l: LocalDateTime => l.toDateTime
   }
 }
