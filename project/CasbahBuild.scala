63114898825bb5a86e030afc7ad746e84f1e2e56
Ross Lawley <ross.lawley@gmail.com>
2013-08-27 13:16:39 +0100

    Scala version and sbt update

--- project/CasbahBuild.scala
+++ project/CasbahBuild.scala
@@ -16,7 +16,7 @@ object CasbahBuild extends Build {
     organization := "org.mongodb",
     organizationHomepage := Some(url("http://www.mongodb.org")),
     version      := "2.6.3-SNAPSHOT",
-    scalaVersion := "2.10.1",
+    scalaVersion := "2.10.2",
     crossScalaVersions := Seq("2.10.2", "2.10.1", "2.10.0", "2.9.3", "2.9.2", "2.9.1")
   )
 
