8ac56d22849be0e012cf5eb75285c266be381fef
Ross Lawley <ross.lawley@gmail.com>
2014-03-13 09:15:18 +0000

    Bump to Casbah-2.7.0-RC2

--- project/CasbahBuild.scala
+++ project/CasbahBuild.scala
@@ -15,7 +15,7 @@ object CasbahBuild extends Build {
   lazy val buildSettings = Seq(
     organization := "org.mongodb",
     organizationHomepage := Some(url("http://www.mongodb.org")),
-    version      := "2.7.0-RC1",
+    version      := "2.7.0-RC2",
     scalaVersion := "2.10.3",
     crossScalaVersions := Seq("2.10.3", "2.9.3")
   )
@@ -140,7 +140,7 @@ object CasbahBuild extends Build {
 object Dependencies {
 
   //val mongoJavaDriver  = "org.mongodb" % "mongo-java-driver" % "3.0.0-SNAPSHOT"
-  val mongoJavaDriver  = "org.mongodb" % "mongo-java-driver" % "2.12.0-rc1"
+  val mongoJavaDriver  = "org.mongodb" % "mongo-java-driver" % "2.12.0-rc2"
   val slf4j            = "org.slf4j" % "slf4j-api" % "1.6.0"
   val junit            = "junit" % "junit" % "4.10" % "test"
   val slf4jJCL         = "org.slf4j" % "slf4j-jcl" % "1.6.0" % "test"
