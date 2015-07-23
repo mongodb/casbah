750214547827b4a28a25661b9a50d3d1e7069697
Ross Lawley <ross.lawley@gmail.com>
2015-07-23 09:55:33 +0100

    Fix scalastyle

--- project/CasbahBuild.scala
+++ project/CasbahBuild.scala
@@ -160,7 +160,7 @@ object Dependencies {
 
   def scalaStyle(scalaVersion: String) =
     scalaVersion match {
-      case "2.11.5"   => Seq()
+      case "2.11.7"   => Seq()
       case _ => Seq("org.scalastyle" %% "scalastyle" % "0.4.0"  % "test")
     }
 }
